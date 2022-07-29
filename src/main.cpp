e8ac7e687e5a72ea88ac780003fae2693245a763
MasterCoookie <jas.kocurek2@wp.pl>
2022-07-29 14:28:26 +0200

    definitions and declarations

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,28 @@
 #include <Arduino.h>
 #include <Stepper.h>
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
 
 const int stepsPerRevolution = 2048;
+enum class RotorState { UP, STOP, DOWN };
 
 #define IN1 13
 #define IN2 12
 #define IN3 14
 #define IN4 27
 
+#define SERVICE_UUID        "eda3620e-0e6a-11ed-861d-0242ac120002"
+#define CHARACTERISTIC_UUID "f67783e2-0e6a-11ed-861d-0242ac120002"
+
 Stepper myStepper(stepsPerRevolution, IN1, IN3, IN2, IN4);
 
 void setup() {
-  // set the speed at 5 rpm
-  myStepper.setSpeed(10);
+  myStepper.setSpeed(17);
   // initialize the serial port
   Serial.begin(115200);
 }
 
 void loop() {
- Serial.println("clockwise");
-  myStepper.step(stepsPerRevolution);
-  delay(1000);
 
-  // step one revolution in the other direction:
-  Serial.println("counterclockwise");
-  myStepper.step(-stepsPerRevolution);
-  delay(1000);
 }
